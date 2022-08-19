0f465238728b77a739797820ce46e22ffe0f1603
AldairGarciaM <0210aldairgarcia@gmail.com>
2022-08-18 20:35:54 -0500

    Actualizacion

--- EduMoocks/EduMoocks/settings.py
+++ EduMoocks/EduMoocks/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'admin_interface',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -41,9 +42,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'contenido',
     'principal.apps.PrincipalConfig',
+    'colorfield',
     
 
 ]
+X_FRAME_OPTIONS='SAMEORIGIN'
 
 CRISPY_TEMPLATE_PACK= 'bootstrap 4'
 
